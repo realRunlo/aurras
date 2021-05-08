d26608bf23e7526e1a835d879183e3a9bfcc0168
realRunlo <gteixeira.pereira@gmail.com>
2021-05-08 16:23:11 +0100

    fisrt commit,init project

---
+++ src/aurras.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+/* Client, to run client ./aurras -> show how to use, ./aurras status -> show status , ./aurras transform [....] -> submit request */
+int main(int argc,char * argv[]){
+
+
+
+    return 0;
+}
---
+++ src/aurrasd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+/* Server, to run server ./aurrasd config-filename filter-folder*/
+int main(int argc,char * argv[]){
+
+
+
+    return 0;
+}
