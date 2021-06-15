f6911fa08ad3659791952d33887293f57be9a4ba
realRunlo <gteixeira.pereira@gmail.com>
2021-06-15 14:06:15 +0100

    fixed some bugs

--- src/program/aurrasd.c
+++ src/program/aurrasd.c
@@ -61,14 +61,12 @@ void update_handler(int signum){
     remove(filename);
     printf("updated\n");
     kill(atoi(getId(req)),SIGINT); //terminate client because finisihed processing audio
-    
-
 }
 
 void close_handler(int signum){
     close(c2s_pipe);
     remove("tmp/client2server");
-;
+
     //printf("\nServer offline.\n");
 }
 
@@ -77,14 +75,7 @@ int main(int argc,char * argv[]){
     signal(SIGUSR1,update_handler);
    // signal(SIGINT,close_handler);
     pipe(updatePipe);
-    /*
-    filters = NULL;
-    waiting_queque = NULL;
-    runing_queque = NULL;
-    filters_toExec = NULL;*/
-
     pid_father = getpid();
-    printf("%d\n",pid_father);
     int task_counter = 1;
     int r_size;
     char * filters_folder;
@@ -126,7 +117,6 @@ int main(int argc,char * argv[]){
             sprintf(pipeName,"tmp/%s",getId(req));
             s2c_pipe = open(pipeName,O_WRONLY);
             dup2(s2c_pipe,1);
-            printf("%d---%d\n",get_sizel(runing_queque),get_sizel(filters));
             show_taskList(runing_queque);
             show_filterList(filters);
             dup2(saveSTDOUT,1);
@@ -162,49 +152,33 @@ int main(int argc,char * argv[]){
                     int pids[n_filtersToExec];
                     int pid,res_pipe=0;
                     int arrayPipes[n_filtersToExec-1][2];
-                    printf("%d\n",n_filtersToExec);
+
                     for(int i=0;i<n_filtersToExec;i++){//launch all de execs
                         
                         Filter exF = (Filter) getValue(filters_toExec,i);
                         char * execName = get_exec_name(exF);
-                        printf("%s\n",execName);
                         char folder[100];
                         sprintf(folder,"%s/%s",filters_folder,execName);
 
-                        if(n_filtersToExec==1){ //only one filter is aplied
-                            switch (pid = fork())
-                            {
-                            case -1:
-                                perror("fork");
-                                _exit(-1);
-                            case 0:
-                                dup2(inFp,0); 
-                                dup2(outFp,1);
-                                execlp(folder,execName,NULL);
-                                _exit(-1);
-                            default:
-                               pids[i] = pid;
-                                
+                        if(i==0){ // first exec
+                            pipe(arrayPipes[i]);
+                            if(res_pipe==-1){
+                                printf("Error pipe %d:",i);
                             }
 
-
-                        }else{
-                            if(i==0){ // first exec
-                                pipe(arrayPipes[i]);
-                                if(res_pipe==-1){
-                                    printf("Error pipe %d:",i);
-                                }
-
-                                switch (pid = fork())
-                                {
+                            switch (pid = fork())
+                            {
                                 case -1:
                                     perror("fork");
                                     _exit(-1);
                                  case 0:
                                     close(arrayPipes[i][0]);
-                                    dup2(arrayPipes[i][1],1);
-                                    close(arrayPipes[i][1]);
-                                    dup2(inFp,0); 
+                                    dup2(inFp,0);
+                                    if(n_filtersToExec==1) dup2(outFp,1);
+                                    else{
+                                        dup2(arrayPipes[i][1],1);
+                                        close(arrayPipes[i][1]);
+                                    } 
                                     execlp(folder,execName,NULL);
                                     exit(-1);
                                 default:
@@ -212,50 +186,43 @@ int main(int argc,char * argv[]){
                                 
                                 }
 
-                            }else if(i==n_filtersToExec-1){ // last exec
-
-                                switch (pid = fork())
-                                {
-                                case -1:
-                                    perror("fork");
-                                    _exit(-1);
-                                case 0:
-                                    dup2(arrayPipes[i-1][0],0);
-                                    close(arrayPipes[i-1][0]);
-                                    dup2(outFp,1);// prints de output in the output file
-                                    execlp(folder,execName,NULL);
-                                    _exit(-1);
-                                default:
-                                    close(arrayPipes[i-1][1]);
-                                
-                                }
-                            }else{ // mid execs
-                                switch (pid = fork())
-                                {
-                                case -1:
-                                    perror("fork");
-                                    _exit(-1);
-                                case 0:
-                                    close(arrayPipes[i][0]);
-                                    dup2(arrayPipes[i-1][0],0);
-                                    close(arrayPipes[i-1][0]);     
-                                    dup2(arrayPipes[i][1],1);
-                                    close(arrayPipes[i][1]);
-                                    execlp(folder,execName,NULL);
-                                    _exit(-1);
-                                default:
-                                   close(arrayPipes[i][1]);
-                                   close(arrayPipes[i-1][0]);
-                                
-                               }
-
+                        }else if(i==n_filtersToExec-1){ // last exec
+                            switch (pid = fork())
+                            {
+                            case -1:
+                                perror("fork");
+                                _exit(-1);
+                            case 0:
+                                dup2(arrayPipes[i-1][0],0);
+                                close(arrayPipes[i-1][0]);
+                                dup2(outFp,1);
+                                execlp(folder,execName,NULL);
+                                _exit(-1);
+                            default:
+                                close(arrayPipes[i-1][1]);
                             }
-
-                       
-                            pids[i] = pid; //store pid of the processes launched to exec
-                        
+                        }else{ // mid execs
+                            pipe(arrayPipes[i]);
+                            switch (pid = fork())
+                            {
+                            case -1:
+                                perror("fork");
+                                _exit(-1);
+                            case 0:
+                                close(arrayPipes[i][0]);
+                                dup2(arrayPipes[i-1][0],0);
+                                close(arrayPipes[i-1][0]);     
+                                dup2(arrayPipes[i][1],1);
+                                close(arrayPipes[i][1]);
+                                execlp(folder,execName,NULL);
+                                _exit(-1);
+                            default:
+                                close(arrayPipes[i][1]);
+                                close(arrayPipes[i-1][0]);
+                                
+                            }
                         }
-
+                        pids[i] = pid; //store pid of the processes launched to exec
                     }
             
                        
